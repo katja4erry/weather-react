a3ba578b4db5733c0484f5abfe3d3203ea0dc6eb
katja4erry <106451546+katja4erry@users.noreply.github.com>
2022-09-18 15:01:41 +0200

    add open ssource link

---
+++ src/Link.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Link.css";
+
+export default function Link() {
+  return (
+    <div className="Link-source">
+      <a
+        href="https://github.com/katja4erry/weather-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code{" "}
+      </a>{" "}
+      by Ekateryna Myhalchenko
+    </div>
+  );
+}
